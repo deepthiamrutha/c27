a85a6ee01c7df16f5811e5435bf679a3347ef8c1
deepthiamrutha <deepthiamrutha4@gmail.com>
2020-08-06 15:39:47 +0530

    added chain

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImg,platform,log6,chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -35,6 +35,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log6 =new Log(150,290,50,PI/2);
+    chain=new Chain(bird.body,log6.body);
 
 }
 
@@ -61,4 +63,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    log6.display();
+    chain.display();
 }
